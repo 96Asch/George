adf6a77d42ef4a20d72f5d63bd24e3ec0187f8af
adamsdl3 <adamsdl3@miamioh.edu>
2014-11-09 19:58:04 -0500

    Added AlphaBeta
    Added AlphaBeta

--- GeorgeClobberPlayer.java
+++ GeorgeClobberPlayer.java
@@ -202,6 +202,191 @@ public class GeorgeClobberPlayer extends GamePlayer {
 		}
 	}
 	
+	private void alphaBeta(ClobberState brd, int currDepth, double alpha, double beta)
+	{
+		boolean toMaximize = (brd.getWho() == GameState.Who.HOME);
+		boolean toMinimize = !toMaximize;
+		boolean isTerminal = terminalValue(brd, mvStack[currDepth]);
+		
+		if (isTerminal) {
+			;
+		} else if (currDepth == depthLimit) {
+			mvStack[currDepth].set(new ClobberMove(), evalBoard(brd));
+		} else {
+			ScoredClobberMove tempMv = new ScoredClobberMove(new ClobberMove(), 0);
+
+			double bestScore = (toMaximize ? Double.NEGATIVE_INFINITY :
+												Double.POSITIVE_INFINITY);
+			ScoredClobberMove bestMove = mvStack[currDepth];
+			ScoredClobberMove nextMove = mvStack[currDepth+1];
+			
+			bestMove.set(new ClobberMove(), bestScore);
+			GameState.Who currTurn = brd.getWho();
+			char mySymbol;
+			char oppSymbol;
+			
+			if(currTurn == Who.AWAY){
+				mySymbol = ClobberState.awaySym;
+				oppSymbol = ClobberState.homeSym;
+			}
+			else {
+				mySymbol = ClobberState.homeSym;
+				oppSymbol = ClobberState.awaySym;
+			}
+			
+			for(int i = 0; i < ClobberState.ROWS; i++){
+				for(int j = 0; j < ClobberState.COLS; j++){
+					if(brd.board[i][j] == mySymbol){
+						if(i > 0 && brd.board[i-1][j] == oppSymbol){
+							tempMv.row1 = i;
+							tempMv.col1 = j;
+							tempMv.row2 = i - 1;
+							tempMv.col2 = j;
+							brd.makeMove(tempMv);
+
+							alphaBeta(brd, currDepth+1, alpha, beta);
+							
+							//Undo move
+							brd.board[i][j] = mySymbol;
+							brd.board[i-1][j] = oppSymbol;
+							brd.numMoves--;
+							brd.status = GameState.Status.GAME_ON;
+							brd.who = currTurn;
+
+							//Check Results
+							if (toMaximize && nextMove.score > bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							} else if (!toMaximize && nextMove.score < bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							}
+							
+							// Update alpha and beta. Perform pruning, if possible.
+							if (toMinimize) {
+								beta = Math.min(bestMove.score, beta);
+								if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+									return;
+								}
+							} else {
+								alpha = Math.max(bestMove.score, alpha);
+								if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+									return;
+								}
+							}
+						}
+						if(i < ClobberState.ROWS -1 && brd.board[i+1][j] == oppSymbol){
+							tempMv.row1 = i;
+							tempMv.col1 = j;
+							tempMv.row2 = i + 1;
+							tempMv.col2 = j;
+							brd.makeMove(tempMv);
+
+							alphaBeta(brd, currDepth+1, alpha, beta);
+
+							//Undo move
+							brd.board[i][j] = mySymbol;
+							brd.board[i+1][j] = oppSymbol;
+							brd.numMoves--;
+							brd.status = GameState.Status.GAME_ON;
+							brd.who = currTurn;
+
+							//Check Results
+							if (toMaximize && nextMove.score > bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							} else if (!toMaximize && nextMove.score < bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							}
+							
+							// Update alpha and beta. Perform pruning, if possible.
+							if (toMinimize) {
+								beta = Math.min(bestMove.score, beta);
+								if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+									return;
+								}
+							} else {
+								alpha = Math.max(bestMove.score, alpha);
+								if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+									return;
+								}
+							}
+						}
+						if(j > 0 && brd.board[i][j-1] == oppSymbol){
+							tempMv.row1 = i;
+							tempMv.col1 = j;
+							tempMv.row2 = i;
+							tempMv.col2 = j - 1;
+							brd.makeMove(tempMv);
+
+							alphaBeta(brd, currDepth+1, alpha, beta);
+							
+							//Undo move
+							brd.board[i][j] = mySymbol;
+							brd.board[i][j-1] = oppSymbol;
+							brd.numMoves--;
+							brd.status = GameState.Status.GAME_ON;
+							brd.who = currTurn;
+
+							//Check Results
+							if (toMaximize && nextMove.score > bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							} else if (!toMaximize && nextMove.score < bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							}
+							
+							// Update alpha and beta. Perform pruning, if possible.
+							if (toMinimize) {
+								beta = Math.min(bestMove.score, beta);
+								if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+									return;
+								}
+							} else {
+								alpha = Math.max(bestMove.score, alpha);
+								if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+									return;
+								}
+							}
+						}
+						if(j < ClobberState.COLS -1 && brd.board[i][j+1] == oppSymbol){
+							tempMv.row1 = i;
+							tempMv.col1 = j;
+							tempMv.row2 = i;
+							tempMv.col2 = j + 1;
+							brd.makeMove(tempMv);
+
+							alphaBeta(brd, currDepth+1, alpha, beta);
+							
+							//Undo move
+							brd.board[i][j] = mySymbol;
+							brd.board[i][j+1] = oppSymbol;
+							brd.numMoves--;
+							brd.status = GameState.Status.GAME_ON;
+							brd.who = currTurn;
+
+							//Check Results
+							if (toMaximize && nextMove.score > bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							} else if (!toMaximize && nextMove.score < bestMove.score) {
+								bestMove.set(tempMv, nextMove.score);
+							}
+							
+							// Update alpha and beta. Perform pruning, if possible.
+							if (toMinimize) {
+								beta = Math.min(bestMove.score, beta);
+								if (bestMove.score <= alpha || bestMove.score == -MAX_SCORE) {
+									return;
+								}
+							} else {
+								alpha = Math.max(bestMove.score, alpha);
+								if (bestMove.score >= beta || bestMove.score == MAX_SCORE) {
+									return;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	public double evalBoard(ClobberState brd){
 		double score = 0;
 		char mySymbol;
@@ -284,7 +469,7 @@ public class GeorgeClobberPlayer extends GamePlayer {
 	
 	public GameMove getMove(GameState state, String lastMove)
 	{
-		minimax((ClobberState)state, 0);
+		alphaBeta((ClobberState)state, 0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
 		return mvStack[0];
 	}
 	
